3b53bc8a6d035d0667b65c6faf690d407dfb7884
Angel <angel.manilla.2002@gmail.com>
2024-10-07 22:04:54 -0600

    Feat : add with and height  luis and santiago

--- kevin_luis/View/CalculatorView.py
+++ kevin_luis/View/CalculatorView.py
@@ -2,8 +2,12 @@ from tkinter import *
 from math import *
 
 def createWindow():
+    window = Tk()
+    window.title("Calculator")
+    window.geometry('800x800')
+    window.mainloop()
 
-def initComponentWindow():
+"""def initComponentWindow():
 
 def onClickButton():
 
@@ -11,5 +15,7 @@ def onChangeSwitch():
 
 def showError():
     #notificar al usuario
-    # consola
+    # consola"""
 
+if __name__ == '__main__':
+    createWindow()
