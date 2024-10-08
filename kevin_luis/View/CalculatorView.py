2f009b6d6a54a6d452e7a02ab8ab055f83d9f046
Angel <angel.manilla.2002@gmail.com>
2024-10-08 16:12:34 -0600

    Feat : add buttom plus and window 800x800

--- kevin_luis/View/CalculatorView.py
+++ kevin_luis/View/CalculatorView.py
@@ -1,19 +1,31 @@
 from tkinter import *
 from math import *
+import tkinter as tk
+from tkinter import ttk,messagebox
+
 
 def createWindow():
-    window = Tk()
+    window = tk.Tk()
+    data = ttk.Entry(window)
+    bottomAddNum = ttk.Button(text="+",width=10)
+    #title
     window.title("Calculator")
     window.geometry('800x800')
-    window.mainloop()
 
-def initComponentWindow():
+    #position
+    bottomAddNum.place(x = 100, y= 100)
+    window.mainloop()
+#def initComponentWindow(window):
+   
+
+
 
 def onClickButton():
 
-def onChangeSwitch():
 
-def showError():
+#def onChangeSwitch():
+
+#def showError():
     #notificar al usuario
     # consola"""
 
